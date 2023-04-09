2dcc7607d1e308d1d81b9eb104935901e68ce4e0
Katia <kushnir.ekaterina@gmail.com>
2023-04-09 20:14:55 +0200

    Add next.config.js and update deploy script

--- next.config.js
+++ next.config.js
@@ -9,8 +9,5 @@ module.exports = {
   // Export the site as a static site
   target: "experimental-serverless-trace",
   // Specify the output directory for the exported files
-  output: {
-    path: `${__dirname}/out`,
-    publicPath: "katiaku.github.io",
-  },
+  output: "export"
 };
