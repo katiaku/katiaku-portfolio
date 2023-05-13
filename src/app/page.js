fcf7f6d92b3a06dae33356ff1d188db110dfaf00
Katia <kushnir.ekaterina@gmail.com>
2023-05-13 16:36:37 +0200

    Add BAck To Top button

--- src/app/page.js
+++ src/app/page.js
@@ -24,7 +24,7 @@ export default function Home() {
       </Head>
 
       <main className='bg-white px-10 pb-20 md:px-20 lg:px-40 dark:bg-gray-900'>
-        <section className='min-h-screen pb-10'>
+        <section id="top" className='min-h-screen pb-10'>
           <nav className='py-10 mb-12'>
             <ul className='flex items-center gap-5'>
               <li className='mr-auto shake'>
@@ -118,6 +118,10 @@ export default function Home() {
             </div>
           </div>
         </section>
+
+        <footer className='my-2 flex justify-center'>
+          <a className="bg-gradient-to-r from-purple-600 to-blue-500 text-white px-4 py-2 leading-8 rounded-md hover:ring-4 hover:ring-indigo-300" href="#">Back To Top</a>
+        </footer>
       </main>
     </div>
   )
