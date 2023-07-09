bfabde74a21deb487f028342f567ce7986e90929
Katia <kushnir.ekaterina@gmail.com>
2023-07-09 17:15:32 +0200

    Change rounded to md in tech and portfolio

--- src/app/page.js
+++ src/app/page.js
@@ -63,7 +63,7 @@ export default function Home() {
             <h3 className='text-3xl py-1 text-purple-600 font-bold text-center'>Technologies and Tools I Use</h3>
           </div>
           <div className='lg:flex lg:justify-center lg:gap-10'>
-            <div className='wobble text-center shadow-lg p-10 rounded-2xl my-10 dark:bg-white'>
+            <div className='wobble text-center shadow-lg p-10 rounded-md my-10 dark:bg-white'>
               <h3 className='text-lg font-semibold py-2 text-blue-500'>Frontend</h3>
               {/* <Image src={} width={100} height={100} /> */}
               <p className='text-gray-800 py-1'>HTML</p>
@@ -76,7 +76,7 @@ export default function Home() {
               <p className='text-gray-800 py-1'>ReactJS</p>
               <p className='text-gray-800 py-1'>jQuery</p>
             </div>
-            <div className='wobble text-center shadow-lg p-10 rounded-2xl my-10 dark:bg-white'>
+            <div className='wobble text-center shadow-lg p-10 rounded-md my-10 dark:bg-white'>
               {/* <Image src={} width={100} height={100} /> */}
               <h3 className='text-lg font-semibold py-2 text-blue-500'>Backend</h3>
               <p className='text-gray-800 py-1'>SQL (MySQL, PostgreSQL)</p>
@@ -85,13 +85,13 @@ export default function Home() {
               <p className='text-gray-800 py-1'>Node.js (Express)</p>
               <p className='text-gray-800 py-1'>MongoDB</p>
             </div>
-            <div className='wobble text-center shadow-lg p-10 rounded-2xl my-10 dark:bg-white'>
+            <div className='wobble text-center shadow-lg p-10 rounded-md my-10 dark:bg-white'>
               {/* <Image src={} width={100} height={100} /> */}
               <h3 className='text-lg font-semibold py-2 text-blue-500'>UI/UX</h3>
               <p className='text-gray-800 py-1'>Figma</p>
               <p className='text-gray-800 py-1'>Adobe XD</p>
             </div>
-            <div className='wobble text-center shadow-lg p-10 rounded-2xl my-10 dark:bg-white'>
+            <div className='wobble text-center shadow-lg p-10 rounded-md my-10 dark:bg-white'>
               {/* <Image src={} width={100} height={100} /> */}
               <h3 className='text-lg font-semibold py-2 text-blue-500'>Others</h3>
               <p className='text-gray-800 py-1'>Git</p>
@@ -108,11 +108,11 @@ export default function Home() {
           </div>
           <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
             <div className='basis-1/3 flex-1'>
-              <Image src={project1} alt="project1" className='rounded-2xl object-cover shadow-lg' width={'100%'} height={'100%'} />
+              <Image src={project1} alt="project1" className='rounded-md object-cover shadow-lg' width={'100%'} height={'100%'} />
               <p className='dark:text-white pt-5'><a className="text-blue-500 font-semibold" target="_blank" href="https://github.com/katiaku/task-manager">The Task Manager React App</a> is a web app that allows users to manage their tasks in a simple and efficient way. Users can add new tasks to the list, edit or remove existing tasks. Each task in the app includes a description and a priority level that indicates its relative importance or urgency.</p>
             </div>
             <div className='basis-1/3 flex-1'>
-              <Image src={project2} alt="project2" className='rounded-2xl object-cover shadow-lg' width={'100%'} height={'100%'} />
+              <Image src={project2} alt="project2" className='rounded-md object-cover shadow-lg' width={'100%'} height={'100%'} />
               <p className='dark:text-white pt-5'><a className="text-blue-500 font-semibold" target="_blank" href="https://github.com/katiaku/my-birthday">The CakePHP Birthday Planner for Kids</a> is a web application designed to help parents plan and manage their children&apos;s birthday parties. The app provides an intuitive interface that allows users to organize party details including lists of guests and presents.</p>
             </div>
           </div>
