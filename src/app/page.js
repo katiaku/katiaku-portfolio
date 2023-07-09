db82704e5c1e5c0f50c18683021902652db84509
Katia <kushnir.ekaterina@gmail.com>
2023-07-09 16:55:00 +0200

    Change purple headers to 700

--- src/app/page.js
+++ src/app/page.js
@@ -42,7 +42,7 @@ export default function Home() {
           </nav>
           <div className='text-center p-2 lg:py-24'>
             <h2 className='text-6xl py-2 gradient-text font-extrabold md:text-7xl'>Ekaterina Kushnir</h2>
-            <h3 className='text-2xl font-bold text-blue-900 pt-5 md:text-3xl dark:text-blue-400'>Web Developer</h3>
+            <h3 className='text-2xl font-bold text-purple-700 pt-5 md:text-3xl dark:text-blue-400'>Web Developer</h3>
             <p className='text-left text-md py-8 leading-7 lg:leading-8 text-gray-700 md:text-xl mx-auto dark:text-white'>
               If you&apos;re looking for a versatile web developer who can help you take advantage 
               of the latest advancements in the field, you&apos;ve come to the right place. 
@@ -52,8 +52,8 @@ export default function Home() {
               to more complex web applications. Please take a look around this site to find out more 
               about my skills as well as some examples of my work.</p>
             <div className='flex justify-center gap-5 mt-5'>
-              <a className="bg-gradient-to-r from-purple-600 to-blue-500 text-white px-4 py-2 leading-8 rounded-3xl hover:ring-4 hover:ring-indigo-300" href="https://drive.google.com/file/d/1-gao6ygjUv6HNikOPWnf_mocNk6Dh9id/view?usp=share_link" target="_blank">Resume</a>
-              <a className="bg-gradient-to-r from-purple-600 to-blue-500 text-white px-4 py-2 leading-8 rounded-3xl hover:ring-4 hover:ring-indigo-300" href="mailto:kushnir.ekaterina@gmail.com">Contact Me</a>
+              <a className="bg-gradient-to-r from-purple-700 to-blue-500 text-white px-4 py-2 leading-8 rounded-3xl hover:ring-4 hover:ring-indigo-300" href="https://drive.google.com/file/d/1-gao6ygjUv6HNikOPWnf_mocNk6Dh9id/view?usp=share_link" target="_blank">Resume</a>
+              <a className="bg-gradient-to-r from-purple-700 to-blue-500 text-white px-4 py-2 leading-8 rounded-3xl hover:ring-4 hover:ring-indigo-300" href="mailto:kushnir.ekaterina@gmail.com">Contact Me</a>
             </div>
           </div>
         </section>
@@ -101,7 +101,7 @@ export default function Home() {
 
         <section>
           <div>
-            <h3 className='text-3xl pb-6 pt-18 text-purple-600 text-center font-bold'>My Portfolio</h3>
+            <h3 className='text-3xl pb-6 pt-18 text-purple-700 text-center font-bold'>My Portfolio</h3>
           </div>
           <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
             <div className='basis-1/3 flex-1'>
