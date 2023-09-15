55b8f3cec55ac1996d0d0b544df8cef2d6900925
Katia <kushnir.ekaterina@gmail.com>
2023-09-15 17:19:26 +0200

    Add copyright to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
 export default function Footer() {
     return (
-        <footer className='my-2 mt-8 flex justify-center'>
+        <footer className='mt-8 flex flex-col gap-6 items-center'>
             <a className="btn" href="#">Back To Top</a>
+            <p>Ekaterina Kushnir | Portfolio 2023 &copy;</p>
         </footer>
     )
 }
