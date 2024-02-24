117501d94e24d39d5d83c5974fa8099754501085
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-02-24 17:34:39 +0100

    Final touch

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,9 @@
 
 import Head from 'next/head';
 import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import { IoLogoCss3, IoLogoAngular, IoLogoHtml5 } from "react-icons/io";
+import { FaBootstrap, FaSass, FaJira, FaReact, FaFigma, FaVuejs, FaPhp, FaNodeJs, FaGitAlt, FaGithub, FaGitlab } from "react-icons/fa";
+import { SiJest, SiCypress, SiPostman, SiStorybook, SiTailwindcss, SiTypescript, SiJavascript, SiJquery, SiThreedotjs, SiCakephp, SiExpress, SiMysql, SiMongodb } from "react-icons/si";
 import Image from 'next/image';
 import project1 from '../../public/project1.png';
 import project2 from '../../public/project2.png';
@@ -36,11 +39,10 @@ export default function Home() {
         <link rel="icon" href="icon.png" />
       </Head>
 
-      {/* <main id="main" className='bg-white px-10 pb-10 md:px-20 lg:px-40 dark:bg-gray-900 transition-all duration-300'> */}
       <main id="main" className='relative bg-slate-100 pb-10 dark:bg-gray-900 transition-all duration-300 w-screen'>
 
         <div id="background">
-          <Image src={background} alt="background" />
+          <Image src={background} alt="futuristic background of colored elements" />
         </div>
         
         {/* <Cursor /> */}
@@ -54,7 +56,7 @@ export default function Home() {
           >
             <ul className='flex flex-col sm:flex-row sm:items-center gap-8 '>
               <li>
-                <Image src={logo} alt="logo" width={30} height={30} className='m-3' />
+                <Image src={logo} alt="personal EK logo" width={30} height={30} className='m-3' />
               </li>
               <ul className='flex justify-center items-center gap-10 sm:ml-auto'>
                 <li className='menu-item'><a href="#about">ABOUT</a></li>
@@ -93,7 +95,7 @@ export default function Home() {
         </section>
 
         <section className='mt-10 mb-3 py-20 mx-20 relative' id='about'>
-          <Image src={ellipse1} alt="ellipse1" width="800" height="800" className="ellipse1 absolute" />
+          <Image src={ellipse1} alt="colored decorative ellipse" width="800" height="800" className="ellipse1 absolute" />
           {/* <Image src={ellipse2} alt="ellipse2" width="700" height="700" className="ellipse2 absolute" /> */}
           <div className='pb-3'>
             <h3 className='section-title'>About Me</h3>
@@ -117,49 +119,50 @@ export default function Home() {
         </section>
         
         <section className='pt-18 mb-5 pb-20 px-20 relative' id='tools'>
-          <Image src={ellipse3} alt="ellipse3" width="800" height="800" className="ellipse3 absolute" />
-          <Image src={vector2} alt="vector2" width="700" height="700" className="vector2 absolute" />
+          <Image src={ellipse3} alt="colored decorative ellipse" width="800" height="800" className="ellipse3 absolute" />
+          <Image src={vector2} alt="colored decorative ellipse" width="700" height="700" className="vector2 absolute" />
           <div className='pb-3'>
             <h3 className='section-title'>Technologies And Tools I Use</h3>
           </div>
           <div className='lg:flex lg:justify-between gap-6'>
             <div className='tech-group glass'>
-              <Image src={front} width={100} height={100} />
+              <Image src={front} width={100} height={100} alt="frontend development icon" />
               <h3 className='tech-group-title'>Frontend</h3>
-              <p className='tech-item'>HTML</p>
-              <p className='tech-item'>CSS</p>
-              <p className='tech-item'>Scss</p>
-              <p className='tech-item'>Bootstrap</p>
-              <p className='tech-item'>Tailwind</p>
-              <p className='tech-item'>JavaScript</p>
-              <p className='tech-item'>TypeScript</p>
-              <p className='tech-item'>Angular</p>
-              <p className='tech-item'>React</p>
-              <p className='tech-item'>Vue</p>
-              <p className='tech-item'>jQuery</p>
-              <p className='tech-item'>Three.js</p>
+              <div className="tech-subj"><IoLogoHtml5 className="text-xl"/><p className='tech-item'>HTML</p></div>
+              <div className="tech-subj"><IoLogoCss3 className="text-xl" /><p className='tech-item'>CSS</p></div>
+              <div className="tech-subj"><FaSass className="text-xl" /><p className='tech-item'>Sass</p></div>
+              <div className="tech-subj"><FaBootstrap className="text-xl" /><p className='tech-item'>Bootstrap</p></div>
+              <div className="tech-subj"><SiTailwindcss className="text-xl" /><p className='tech-item'>Tailwind</p></div>
+              <div className="tech-subj"><SiJavascript className="text-x" /><p className='tech-item'>JavaScript</p></div>
+              <div className="tech-subj"><SiTypescript className="text-x" /><p className='tech-item'>TypeScript</p></div>
+              <div className="tech-subj"><IoLogoAngular className="text-xl" /><p className='tech-item'>Angular</p></div>
+              <div className="tech-subj"><FaReact className="text-xl" /><p className='tech-item'>React</p></div>
+              <div className="tech-subj"><FaVuejs className="text-xl" /><p className='tech-item'>Vue</p></div>
+              <div className="tech-subj"><SiJquery className="text-x" /><p className='tech-item'>jQuery</p></div>
+              <div className="tech-subj"><SiThreedotjs className="text-x" /><p className='tech-item'>Three.js</p></div>
             </div>
             <div className='tech-group glass'>
-              <Image src={back} width={100} height={100} />
+              <Image src={back} width={100} height={100} alt="backend developmen icon" />
               <h3 className='tech-group-title'>Backend</h3>
-              <p className='tech-item'>PHP</p>
-              <p className='tech-item'>CakePHP</p>
-              <p className='tech-item'>Node.js</p>
-              <p className='tech-item'>Express</p>
-              <p className='tech-item'>MySQL</p>
-              <p className='tech-item'>MongoDB</p>
+              <div className="tech-subj"><FaPhp className="text-xl" /><p className='tech-item'>PHP</p></div>
+              <div className="tech-subj"><SiCakephp className="text-l" /><p className='tech-item'>CakePHP</p></div>
+              <div className="tech-subj"><FaNodeJs className="text-xl" /><p className='tech-item'>Node.js</p></div>
+              <div className="tech-subj"><SiExpress className="text-xl" /><p className='tech-item'>Express.js</p></div>
+              <div className="tech-subj"><SiMysql className="text-xl" /><p className='tech-item'>MySQL</p></div>
+              <div className="tech-subj"><SiMongodb className="text-xl" /><p className='tech-item'>MongoDB</p></div>
             </div>
             <div className='tech-group glass'>
-              <Image src={others} width={90} height={90} className="pt-1" />
+              <Image src={others} width={90} height={90} className="pt-1" alt="web development icon" />
               <h3 className='tech-group-title'>Others</h3>
-              <p className='tech-item'>Jest</p>
-              <p className='tech-item'>Cypress</p>
-              <p className='tech-item'>Postman</p>
-              <p className='tech-item'>Git</p>
-              <p className='tech-item'>GitHub</p>
-              <p className='tech-item'>GitLab</p>
-              <p className='tech-item'>Figma</p>
-              <p className='tech-item'>JIRA</p>
+              <div className="tech-subj"><SiJest className="text-x" /><p className='tech-item'>Jest</p></div>
+              <div className="tech-subj"><SiCypress className="text-x" /><p className='tech-item'>Cypress</p></div>
+              <div className="tech-subj"><SiPostman className="text-x" /><p className='tech-item'>Postman</p></div>
+              <div className="tech-subj"><FaGitAlt className="text-xl" /><p className='tech-item'>Git</p></div>
+              <div className="tech-subj"><FaGithub className="text-x" /><p className='tech-item'>GitHub</p></div>
+              <div className="tech-subj"><FaGitlab className="text-x" /><p className='tech-item'>GitLab</p></div>
+              <div className="tech-subj"><SiStorybook className="text-x" /><p className='tech-item'>Storybook</p></div>
+              <div className="tech-subj"><FaFigma className="text-x" /><p className='tech-item'>Figma</p></div>
+              <div className="tech-subj"><FaJira className="text-x" /><p className='tech-item'>JIRA</p></div>
             </div>
           </div>
         </section>
@@ -170,7 +173,7 @@ export default function Home() {
           </div>
           <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap mx-20'>
             <div className='basis-1/3 flex-1'>
-              <Image src={jscodegamer} alt="jscodegamer" className='hover-scale rounded-3xl object-cover shadow-lg' width={'100%'} height={'100%'} />
+              <Image src={jscodegamer} alt="jscodegamer app screenshot" className='hover-scale rounded-3xl object-cover shadow-lg' width={'100%'} height={'100%'} />
               <div className='flex gap-2 pt-4'>
                 <span className='bg-blue-500 text-white text-xs rounded-xl px-2 py-1'>Angular</span>
                 <span className='bg-blue-500 text-white text-xs rounded-xl px-2 py-1'>Express.js</span>
@@ -179,7 +182,7 @@ export default function Home() {
               <p className='dark:text-white pt-2'><a className="text-blue-500 dark:text-blue-400 font-bold" target="_blank" href="https://jscodegamer.vercel.app">JSCODEGAMER</a> facilitates learning programming by providing interactive lessons and practical challenges in JavaScript. Its space-inspired environment creates an educational and motivating experience for users who want to embark on their programming journey. The app includes 9 levels that unlock progressively based on the user&apos;s progress, along with diverse challenges and an integrated code editor.</p>
             </div>
             <div className='basis-1/3 flex-1'>
-              <Image src={project1} alt="project1" className='hover-scale rounded-3xl object-cover shadow-lg' width={'100%'} height={'100%'} />
+              <Image src={project1} alt="task manager app screenshot" className='hover-scale rounded-3xl object-cover shadow-lg' width={'100%'} height={'100%'} />
               <div className='flex gap-2 pt-4'>
                 <span className='bg-blue-500 text-white text-xs rounded-xl px-2 py-1'>React</span>
                 <span className='bg-blue-500 text-white text-xs rounded-xl px-2 py-1'>Bootstrap</span>
@@ -187,7 +190,7 @@ export default function Home() {
               <p className='dark:text-white pt-2'><a className="text-blue-500 dark:text-blue-400 font-bold" target="_blank" href="https://github.com/katiaku/task-manager">Task Manager App</a> is a web app that allows users to manage their tasks in a simple and efficient way. Users can add new tasks to the list, edit or remove existing tasks. Each task in the app includes a description and a priority level that indicates its relative importance or urgency.</p>
             </div>
             <div className='basis-1/3 flex-1'>
-              <Image src={project2} alt="project2" className='hover-scale rounded-3xl object-cover shadow-lg' width={'100%'} height={'100%'} />
+              <Image src={project2} alt="birthday planner app screenshot" className='hover-scale rounded-3xl object-cover shadow-lg' width={'100%'} height={'100%'} />
               <div className='flex gap-2 pt-4'>
                 <span className='bg-blue-500 text-white text-xs rounded-xl px-2 py-1'>CakePHP</span>
                 <span className='bg-blue-500 text-white text-xs rounded-xl px-2 py-1'>MySQL</span>
