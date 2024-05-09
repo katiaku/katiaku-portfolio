1d50430f7b44bab5b97ff0d60affb42c3bf1f6af
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-05-09 11:52:03 +0200

    Add KanbanVue project to the portfolio

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,7 @@ import Image from 'next/image';
 import project1 from '../../public/project1.png';
 import project2 from '../../public/project2.png';
 import jscodegamer from '../../public/jscodegamer_game.png';
+import kanbanVue from '../../public/kanbanVue.png';
 import logo from './icon.png';
 import { useState } from 'react';
 import "@theme-toggles/react/css/Expand.css";
@@ -172,6 +173,7 @@ export default function Home() {
             <h3 className='section-title'>My Projects</h3>
           </div>
           <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap mx-20'>
+
             <div className='basis-1/3 flex-1'>
               <Image src={jscodegamer} alt="jscodegamer app screenshot" className='hover-scale rounded-3xl object-cover shadow-lg' width={'100%'} height={'100%'} />
               <div className='flex gap-2 pt-4'>
@@ -181,6 +183,7 @@ export default function Home() {
               </div>
               <p className='dark:text-white pt-2'><a className="text-blue-500 dark:text-blue-400 font-bold" target="_blank" href="https://jscodegamer.vercel.app">JSCODEGAMER</a> facilitates learning programming by providing interactive lessons and practical challenges in JavaScript. Its space-inspired environment creates an educational and motivating experience for users who want to embark on their programming journey. The app includes 9 levels that unlock progressively based on the user&apos;s progress, along with diverse challenges and an integrated code editor.</p>
             </div>
+
             <div className='basis-1/3 flex-1'>
               <Image src={project1} alt="task manager app screenshot" className='hover-scale rounded-3xl object-cover shadow-lg' width={'100%'} height={'100%'} />
               <div className='flex gap-2 pt-4'>
@@ -189,6 +192,7 @@ export default function Home() {
               </div>
               <p className='dark:text-white pt-2'><a className="text-blue-500 dark:text-blue-400 font-bold" target="_blank" href="https://github.com/katiaku/task-manager">Task Manager App</a> is a web app that allows users to manage their tasks in a simple and efficient way. Users can add new tasks to the list, edit or remove existing tasks. Each task in the app includes a description and a priority level that indicates its relative importance or urgency.</p>
             </div>
+
             <div className='basis-1/3 flex-1'>
               <Image src={project2} alt="birthday planner app screenshot" className='hover-scale rounded-3xl object-cover shadow-lg' width={'100%'} height={'100%'} />
               <div className='flex gap-2 pt-4'>
@@ -198,14 +202,15 @@ export default function Home() {
               </div>
               <p className='dark:text-white pt-2'><a className="text-blue-500 dark:text-blue-400 font-bold" target="_blank" href="https://github.com/katiaku/my-birthday">Birthday Planner for Kids</a> is a web application designed to help parents plan and manage their children&apos;s birthday parties. The app provides an intuitive interface that allows users to organize party details including lists of guests and presents.</p>
             </div>
+
             <div className='basis-1/3 flex-1'>
-              <Image alt="" className='rounded-md object-cover shadow-lg' width={'100%'} height={'100%'} />
+              <Image src={kanbanVue} alt="kanban-vue app screenshot" className='hover-scale rounded-3xl object-cover shadow-lg' width={'100%'} height={'100%'} />
               <div className='flex gap-2 pt-4'>
-                <span className=''></span>
-                <span className=''></span>
-                <span className=''></span>
+                <span className='bg-blue-500 text-white text-xs rounded-xl px-2 py-1'>Nuxt3</span>
+                <span className='bg-blue-500 text-white text-xs rounded-xl px-2 py-1'>TypeScript</span>
+                <span className='bg-blue-500 text-white text-xs rounded-xl px-2 py-1'>Tailwind CSS</span>
               </div>
-              <p className='dark:text-white pt-2'><a className="text-blue-500 dark:text-blue-400 font-bold" target="_blank" href=""></a></p>
+              <p className='dark:text-white pt-2'><a className="text-blue-500 dark:text-blue-400 font-bold" target="_blank" href="https://kanban-vue-ruby.vercel.app/">KanbanVue</a> is a kanban board application for work management. Inspired by the principles of Agile methodologies, I wanted to build my own tool to easily visualize work and prioritize tasks. The user can customize columns and tasks and easily drag them around the board.</p>
             </div>
           </div>
         </section>
