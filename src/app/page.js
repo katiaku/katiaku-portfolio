80448f971c216ed6328cebdbd6e71242c0f1675f
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-05-13 18:53:50 +0200

    Seperate Footer component

--- src/app/page.js
+++ src/app/page.js
@@ -26,6 +26,7 @@ import ellipse3 from '../../public/Ellipse 3.png';
 import vector1 from '../../public/Vector 1.png';
 import vector2 from '../../public/Vector 2.png';
 import vector3 from '../../public/Vector 3.png';
+import Footer from '@/components/Footer';
 
 export default function Home() {
 
@@ -350,11 +351,13 @@ export default function Home() {
           </div>
         </section>
 
-        <footer className='px-20 mt-20 pt-4 flex flex-col items-center dark:text-gray-200'>
+        {/* <footer className='px-20 mt-20 pt-4 flex flex-col items-center dark:text-gray-200'>
             <a className="btn" href="#">Back</a>
             <p className='mt-10 text-sm cursor-default'>Making you vision reality</p>
             <p className='text-sm cursor-default'>&copy; 2024 Ekaterina Kushnir. All rights reserved.</p>
-        </footer>
+        </footer> */}
+
+        <Footer />
       </main>
     </div>
   )
