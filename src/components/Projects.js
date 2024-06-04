60b4cadaa14f29b7878d24fe236a9dc8e16e73a5
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-06-04 14:50:44 +0200

    Update projects section layout

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -36,25 +36,25 @@ export default function Projects() {
     return (
         <section
             id="projects"
-            className="relative scroll-mt-28 snap-start pt-20 mb-20 px-8 md:px-20"
+            className="scroll-mt-28 snap-start py-20 mb-20 px-8 md:px-20"
         >
             <div>
                 <h3 className='section-title uppercase'>{ t("section_header_my_work") }</h3>
             </div>
 
-            <div className='flex flex-col md:gap-32 py-14 xl:mx-[10rem]'>
-                <div className='relative lg:h-[470px]'>
-                    <div className='md:opacity-60 mb-4 md:mb-0 glass p-6 md:w-2/3 md:absolute md:right-0 md:top-8 hover-scale'>
+            <div className='flex flex-col items-center md:gap-10 lg:gap-32 py-14 lg:py-20'>
+                <div className='lg:relative lg:w-[880px] flex lg:block flex-col gap-2 md:gap-8'>
+                    <div className='image-glass overflow-hidden mb-4 md:mb-0 lg:w-[480px] lg:absolute lg:right-0 lg:top-8'>
                         <Image
                             src={kanbanVue}
                             alt={ t("kanban_vue_screenshot") }
-                            className='object-cover'
+                            className='object-cover hover-scale dark:opacity-80'
                             width={'100%'}
                             height={'100%'}
                         />
                     </div>
 
-                    <div className='mb-20 md:mb-0 glass p-6 lg:p-10 md:w-3/5 lg:w-1/2 flex gap-6 lg:gap-10 items-center'>
+                    <div className='mb-20 lg:mb-0 glass p-8 lg:w-[480px] flex gap-6 lg:gap-10 items-center'>
                         <div className='flex flex-col gap-6'>
                             <p className='dark:text-white pt-2 cursor-default'>
                                 <span className="text-slate-800 dark:text-blue-400 font-bold uppercase text-2xl mr-1">KanbanVue</span>{ t("project_description_kanban_vue") }
@@ -99,18 +99,18 @@ export default function Projects() {
                     </div>
                 </div>
 
-                <div className='relative lg:h-[470px]'>
-                    <div className='md:opacity-60 mb-4 md:mb-0 glass p-6 md:w-2/3 md:absolute md:right-0 md:top-8 hover-scale'>
+                <div className='lg:relative lg:w-[880px] flex lg:block flex-col gap-2 md:gap-8'>
+                    <div className='image-glass overflow-hidden mb-4 md:mb-0 lg:w-[480px] lg:absolute lg:right-0 lg:top-8'>
                         <Image
                             src={jsCodeGamer}
                             alt={ t("js_code_gamer_vue_screenshot") }
-                            className='object-cover'
+                            className='object-cover hover-scale dark:opacity-80'
                             width={'100%'}
                             height={'100%'}
                         />
                     </div>
     
-                    <div className='mb-20 md:mb-0 glass p-6 lg:p-10 md:w-3/5 lg:w-1/2 flex gap-6 lg:gap-10 items-center'>
+                    <div className='mb-20 lg:mb-0 glass p-8 lg:w-[480px] flex gap-6 lg:gap-10 items-center'>
                         <div className='flex flex-col gap-6'>
                             <p className='dark:text-white pt-2 cursor-default'>
                                 <span className="text-slate-800 dark:text-blue-400 font-bold uppercase text-2xl mr-1">JSCODEGAMER</span>{ t("project_description_js_code_gamer") }
@@ -155,18 +155,17 @@ export default function Projects() {
                     </div>
                 </div>
 
-                <div className='relative lg:h-[470px]'>
-                    <div className='md:opacity-60 mb-4 md:mb-0 glass p-6 md:w-2/3 md:absolute md:right-0 md:top-8 hover-scale'>
+                <div className='lg:relative lg:w-[880px] flex lg:block flex-col gap-2 md:gap-8'>
+                    <div className='image-glass overflow-hidden mb-4 md:mb-0 lg:w-[480px] lg:absolute lg:right-0 lg:top-8'>
                         <Image
                             src={taskManager}
                             alt={ t("task_manager_screenshot") }
-                            className='object-cover'
+                            className='object-cover hover-scale dark:opacity-80'
                             width={'100%'}
                             height={'100%'}
                         />
                     </div>
-    
-                    <div className='mb-20 md:mb-0 glass p-6 lg:p-10 md:w-3/5 lg:w-1/2 flex gap-6 lg:gap-10 items-center'>
+                    <div className='mb-20 lg:mb-0 glass p-8 lg:w-[480px] flex gap-6 lg:gap-10 items-center'>
                         <div className='flex flex-col gap-6'>
                             <p className='dark:text-white pt-2 cursor-default'>
                                 <a className="text-slate-800 dark:text-blue-400 font-bold uppercase text-2xl mr-1" target="_blank" href="https://github.com/katiaku/task-manager">Task Manager</a>{ t("project_description_task_manager") }
@@ -208,18 +207,18 @@ export default function Projects() {
                     </div>
                 </div>
 
-                <div className='relative lg:h-[470px]'>
-                    <div className='md:opacity-60 mb-4 md:mb-0 glass p-6 md:w-2/3 md:absolute md:right-0 md:top-8 hover-scale'>
+                <div className='lg:relative lg:w-[880px] flex lg:block flex-col gap-2 md:gap-8'>
+                    <div className='image-glass overflow-hidden mb-4 md:mb-0 lg:w-[480px] lg:absolute lg:right-0 lg:top-8'>
                         <Image
                             src={birthdayPlanner}
                             alt={ t("birthday_planner_screenshot") }
-                            className='object-cover'
+                            className='object-cover hover-scale dark:opacity-80'
                             width={'100%'}
                             height={'100%'}
                         />
                     </div>
     
-                    <div className='glass p-6 lg:p-10 md:w-3/5 lg:w-1/2 flex gap-6 lg:gap-10 items-center'>
+                    <div className='mb-20 lg:mb-0 glass p-8 lg:w-[480px] flex gap-6 lg:gap-10 items-center'>
                         <div className='flex flex-col gap-6'>
                             <p className='dark:text-white pt-2 cursor-default'>
                                 <a className="text-slate-800 dark:text-blue-400 font-bold uppercase text-2xl mr-1" target="_blank" href="https://github.com/katiaku/my-birthday">Birthday Planner</a>{ t("project_description_birthday_planner") }
