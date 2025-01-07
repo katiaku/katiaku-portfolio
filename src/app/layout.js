71156b6d31943830ac15accdaa0d2d22533adcb6
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2025-01-07 14:41:34 +0100

    add google analytics script

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,7 @@
 import './globals.css'
+import Script from 'next/script'
+
+const GA_TRACKING_ID = 'G-Q0X9VMJ09F';
 
 export const metadata = {
     title: 'Ekaterina Kushnir | Software Development',
@@ -8,6 +11,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+            <head>
+                {/* Google Analytics Script */}
+                <Script
+                    src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${GA_TRACKING_ID}');
+`}
+                </Script>
+            </head>
             <body>{children}</body>
         </html>
     )
